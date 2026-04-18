a6873f5dba0e8bc9a2867c3b97bbe11c85d288d5
pavmardev <pavelm725@gmail.com>
2026-04-18 13:31:57 +0200

    creating migration table Students

---
+++ src/database/migrations/2026_04_18_110955_create_students_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('university, 90');
+            $table->boolean('is_accepted_by_admin');
+            $table->boolean('is_invited_to_the_team');
+            $table->boolean('is_a_teamleader');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
