d2d345fb172f6ba5b3551b9a788d35b2f046d2f0
pavmardev <pavelm725@gmail.com>
2026-04-28 12:55:55 +0200

    vytvorenie novych modelov Article a Mentor a definovanie vztahov v modeloch

---
+++ src/app/Models/Article.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Article extends Model
+{
+    use SoftDeletes;
+    protected $table = 'articles';
+    protected $primaryKey = 'id';
+    protected $fillable = ['user_id', 'title', 'perex', 'text', 'published_at'];
+
+    protected $hidden = ['updated_at','deleted_at'];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+}
+
---
+++ src/app/Models/Mentor.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Mentor extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'mentors';
+    protected $primaryKey = 'id';
+    protected $fillable = ['user_id', 'description', 'experience', 'expertise'];
+
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+}
