3473bcc88760482bb9c451e615c9e7954bd8c13f
pavmardev <pavelm725@gmail.com>
2026-04-29 14:30:19 +0200

    vytvorenie rest api controllera ChallengeC

---
+++ src/app/Http/Controllers/ChallengeController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Challenge;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ChallengeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $challenges = Challenge::all();
+        return response()->json(['vyzvy' => $challenges], Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $challenge = Challenge::select([
+                'challenges.id',
+                'challenges.program',
+                'challenges.name',
+                'challenges.description',
+                'challenges.reward'])
+            ->with(['attachments:id,path,original_name,size,attachable_id,attachable_type'])
+            ->find($id);
+        return response()->json(['vyzvy' => $challenge], Response::HTTP_OK);
+        /*$challenge = Challenge::find($id);
+        return response()->json(['vyzva' => $challenge], Response::HTTP_OK);*/
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
