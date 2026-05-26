8cbd7f7aa7a032fdcbb1ffb760d63da3ab855b6e
pavmardev <pavelm725@gmail.com>
2026-05-26 11:25:44 +0200

    vytvorenie endpointu POST /auth/challenge/{id}/send-to-commission

--- src/routes/api.php
+++ src/routes/api.php
@@ -52,6 +52,7 @@ Route::prefix('auth')->group(function () {
             Route::post('/challenge/{id}/add-milestone', [ChallengeController::class, 'addMilestone']);
             Route::delete('/milestone/{milestone}', [ChallengeController::class, 'destroyMilestone']);
 
+            Route::post('challenge/{id}/send-to-commission', [ChallengeController::class, 'sendToCommission']);
             Route::post('challenge/{id}/start-realisation', [ChallengeController::class, 'startRealisation']);
             Route::get('/challenges', [ChallengeController::class, 'adminChallengesInfo']);
             Route::post('/challenge/{challenge}/accept', [ChallengeController::class, 'acceptChallenge']);
